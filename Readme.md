a647aedfcd25afafd2d74e1d6254ab166eedc80c
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-03-24 00:28:57 +0100

    added animal graphics, beginning of js script and initial readme with credits

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Hippo, Elephant, Dolphin, Turtle, butterfly, panda icons by Visual Pharm (link: http://icons8.com/)
+monkey by Gnome Project https://wiki.gnome.org
+ladybird by http://www.oxygen-icons.org/
+shark by http://schollidesign.deviantart.com/
+the rest of the icons (29) from https://graphicburger.com
---
+++ js/script.js
@@ -0,0 +1,28 @@
+const game = {
+    cardsOpen = 0,
+    cardsAll = 16,
+    moves = 0,
+    time = 0,
+    comparingFinished = false,
+    gamefinished = false,
+}
+
+const icons = ["tiger", "hippo", "lion", "monkey", "zebra", "crocodile", "antelope", "elephant"];
+
+cards
+
+//call randomiser with icons + icons concatenated
+
+function randomiser(source) { 
+    for (let i = 0; i < game.cardsAll; i++) { 
+
+    }
+}
+
+function card(source) { 
+    this.source = source;
+}
+
+function randIndex(range) { 
+    
+}
