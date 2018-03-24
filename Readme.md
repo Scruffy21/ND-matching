7f8583547a5c1b2d058fb2b2d6654e666bbf0fd2
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-03-24 21:38:55 +0100

    progress

--- Readme.md
+++ Readme.md
@@ -5,3 +5,5 @@ shark by http://schollidesign.deviantart.com/
 the rest of the icons (29) from https://graphicburger.com
 file back1_c.jpg only allowed for non commercial use http://www.zcool.com.cn/u/631452/
 file back2.jpg from http://all-free-download.com/
+
+pear from https://icons8.com
--- js/script.js
+++ js/script.js
@@ -2,13 +2,32 @@ const game = {
     cardsOpen: 0,
     cardsAll: 16,
     moves: 0,
-    stars: 3,
+    pears: 3,
     time: 0,
     comparingFinished: false,
     gamefinished: false,
+    nodeList: [],
+    cards: [],
+    icons: ["ant", "deer", "hippo", "mosquito", "snail", "bear", "dinosaur", "horse", "panda", "spider", "beaver", "dolphin", "kangaroo", "pig", "stork", "bee", "duck", "ladybird", "rabbit", "turtle", "butterfly", "elephant", "leopard", "rhinoceros", "whale", "chicken", "frog", "lion", "seahorse", "wolf", "cow", "giraffe", "llama", "shark", "crab", "gorilla", "monkey", "sheep"],
+    populate: function () {
+        const selection = randomiser(game.icons);
+        selection.forEach(function (el, index) {
+            game.cards.push(new card(el, index));
+            game.nodeList[index].firstElementChild.firstElementChild.src = "images/cards/" + el + ".png";
+            game.nodeList[index].id = "card-" + index;
+            // faces[1].id = "card-" + index;
+        });
+    },    
+    reset: function () { 
+        this.cardsOpen = 0;
+        this.moves = 0;
+        this.pears = 3;
+        this.time = 0;
+        this.cards = [];
+        this.populate();
+    },
 }
 
-const icons = ["ant", "deer", "hippo", "mosquito", "snail", "bear", "dinosaur", "horse", "panda", "spider", "beaver", "dolphin", "kangaroo", "pig", "stork", "bee", "duck", "ladybird", "rabbit", "turtle", "butterfly", "elephant", "leopard", "rhinoceros", "whale", "chicken", "frog", "lion", "seahorse", "wolf", "cow", "giraffe", "llama", "shark", "crab", "gorilla", "monkey", "sheep"];
 
 
 //first get the required number of unique icons
@@ -33,8 +52,26 @@ function randomiser(icons) {
 }
 
 
-function card(source) { 
+
+function card(source, id) { 
     this.source = source;
+    this.id = id;
     this.open = false;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+    for (let i = 0; i < cards.length; i++) { 
+        game.nodeList.push(cards[i]);
+    }
+    game.populate();
+    document.querySelector(".game").addEventListener("click", function () { 
+
+    });
+});
+
+function handleClick(event) { 
+    if (event.target.classList.contains("card-back")) { 
+        event.target.parentElement.id
+    }
+}
